118a00754b31057251b657d2a83abcf8841ec894
Dylan Muzondo <dylanmuzondo72@gmail.com>
2026-06-03 16:44:45 +0200

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-const items=document.querySelectorAll('.reveal');
-const io=new IntersectionObserver(entries=>{entries.forEach(e=>{if(e.isIntersecting)e.target.classList.add('show')})},{threshold:.12});
-items.forEach(item=>io.observe(item));
